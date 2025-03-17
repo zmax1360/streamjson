fc7f068082c669daa4c07cbf2ee9373e9f27dac0
Alpha <zafari.a@gmail.com>
2025-03-17 07:52:01 -0400

    change commit

---
+++ src/test/scala/com/example/jsonstream/t.scala
@@ -0,0 +1,84 @@
+package com.example.jsonstream
+
+import io.vertx.core.json.JsonObject
+import io.vertx.core.parsetools.{JsonEvent, JsonEventType, JsonParser}
+import org.junit.runner.RunWith
+import org.mockito.Mockito._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatestplus.junit.JUnitRunner
+import org.scalatestplus.mockito.MockitoSugar
+import rx.Subscriber
+
+@RunWith(classOf[JUnitRunner])
+class ReactHandlerTest extends AnyFlatSpec with Matchers with MockitoSugar {
+
+  // Mock dependencies
+  val mockSubscriber: Subscriber[JsonObject] = mock[Subscriber[JsonObject]]
+  val mockJsonParser: JsonParser = mock[JsonParser]
+  val mockJsonEvent: JsonEvent = mock[JsonEvent]
+
+  // Test object
+  val reactHandler = new ReactHandler(mockSubscriber)
+
+  // Test 1: begin() should log the start of the JSON stream
+  "ReactHandler" should "log the start of the JSON stream" in {
+    reactHandler.begin()
+    // Verify logging behavior (you can use a logging framework like Logback for actual verification)
+  }
+
+  // Test 2: handle() should process JSON objects and ignore non-object events
+  "ReactHandler" should "process JSON objects and ignore non-object events" in {
+    // Case 1: JSON object event
+    when(mockJsonEvent.isObject).thenReturn(true)
+    when(mockJsonEvent.objectValue()).thenReturn(new JsonObject().put("key", "value"))
+    reactHandler.handle(mockJsonEvent)
+    verify(mockSubscriber).onNext(any[JsonObject])
+
+    // Case 2: Non-object event
+    when(mockJsonEvent.isObject).thenReturn(false)
+    reactHandler.handle(mockJsonEvent)
+    verify(mockSubscriber, times(1)).onNext(any[JsonObject]) // No additional calls
+  }
+
+  // Test 3: handleError() should log the error and notify the subscriber
+  "ReactHandler" should "log errors and notify the subscriber" in {
+    val testException = new RuntimeException("Test error")
+    reactHandler.handleError(testException)
+    // Verify logging behavior
+    verify(mockSubscriber).onError(testException)
+  }
+
+  // Test 4: end() should notify the subscriber that the stream is complete
+  "ReactHandler" should "notify the subscriber when the stream ends" in {
+    reactHandler.end()
+    verify(mockSubscriber).onCompleted()
+  }
+
+  // Test 5: arrayJsonEventHandler should handle different JSON event types
+  "HandlerHelper" should "handle different JSON event types" in {
+    val mockHandler = mock[ReactStreamJsonHandler]
+
+    // Case 1: START_ARRAY event
+    when(mockJsonEvent.`type`()).thenReturn(JsonEventType.START_ARRAY)
+    HandlerHelper.arrayJsonEventHandler(mockJsonParser, mockHandler, mockJsonEvent)
+    verify(mockJsonParser).objectValueMode()
+
+    // Case 2: VALUE event
+    when(mockJsonEvent.`type`()).thenReturn(JsonEventType.VALUE)
+    HandlerHelper.arrayJsonEventHandler(mockJsonParser, mockHandler, mockJsonEvent)
+    verify(mockHandler).handle(mockJsonEvent)
+
+    // Case 3: END_ARRAY event
+    when(mockJsonEvent.`type`()).thenReturn(JsonEventType.END_ARRAY)
+    HandlerHelper.arrayJsonEventHandler(mockJsonParser, mockHandler, mockJsonEvent)
+    verify(mockHandler).end()
+  }
+
+  // Test 6: parseJsonStream should configure the parser correctly
+  "HandlerHelper" should "configure the parser correctly" in {
+    val parser = HandlerHelper.parseJsonStream(mockSubscriber)
+    // Verify that the parser is configured with the correct handlers
+    // (This can be tricky to test directly, so we focus on the behavior instead.)
+  }
+}
