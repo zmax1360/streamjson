c9252f7175306deabdd2c606f90a21ceeac3fa3f
Alpha <zafari.a@gmail.com>
2025-03-24 14:36:30 -0400

    change commit

--- src/main/scala/com/example/jsonstream/ReactStreamjsonHndler.scala
+++ src/main/scala/com/example/jsonstream/ReactStreamjsonHndler.scala
@@ -1,80 +1,86 @@
 package com.example.jsonstream
 
-import io.vertx.core.json.JsonObject
-import io.vertx.core.parsetools.{JsonEvent, JsonEventType, JsonParser}
+import io.vertx.core.json.{JsonEvent, JsonEventType, JsonObject, JsonParser}
 import org.slf4j.LoggerFactory
-import rx.Subscriber
+import rx.lang.scala.Subscriber
 import java.math.BigInteger
 
 trait ReactStreamJsonHandler {
   def begin(): Unit
-  def handle(event: JsonEvent):Unit
-  def handleError(e: Throwable):Unit
-  def end():Unit
+  def handle(event: JsonEvent): Unit
+  def handleError(e: Throwable): Unit
+  def end(): Unit
 }
-
 
 class ReactHandler(subscriber: Subscriber[JsonObject]) extends ReactStreamJsonHandler {
   private val logger = LoggerFactory.getLogger(getClass)
+  private var currentObject: JsonObject = null
+  private var currentKey: String = null
+
   override def begin(): Unit = logger.info("JSON stream started")
+
   override def handle(event: JsonEvent): Unit = {
     event.`type`() match {
-      case JsonEventType.VALUE if event.isNumber =>
-        // Handle large numeric values
-        val numericValue = event.value().toString
-        try {
-          val bigIntValue = new BigInteger(numericValue)
-          logger.debug(s"Handling large numeric value: $bigIntValue")
-          // Create a JsonObject with the numeric value
-          val jsonObject = new JsonObject().put("value", bigIntValue.toString)
-          subscriber.onNext(jsonObject)
-        } catch {
-          case e: NumberFormatException =>
-            logger.error(s"Failed to parse large numeric value: $numericValue", e)
-            subscriber.onError(e)
+      case JsonEventType.START_OBJECT =>
+        currentObject = new JsonObject()
+      case JsonEventType.VALUE =>
+        if (currentObject != null) {
+          if (currentKey == null) {
+            currentKey = event.fieldName()
+          } else {
+            val value = event.value()
+            if (value != null) {
+              try {
+                val stringValue = value.toString;
+                try {
+                  currentObject.put(currentKey, stringValue.toLong)
+                }catch {
+                  case _:NumberFormatException =>
+                    currentObject.put(currentKey, new BigInteger(stringValue))
+                }
+              } catch {
+                case e:Exception => currentObject.put(currentKey, value);
+              }
+            } else {
+              currentObject.put(currentKey, null);
+            }
+            currentKey = null
+          }
         }
-      case JsonEventType.VALUE if event.isObject =>
-        // Handle JSON objects as before
-        logger.debug(s"Handling JSON object: ${event.objectValue().toString}")
-        subscriber.onNext(event.objectValue())
+      case JsonEventType.END_OBJECT =>
+        if (currentObject != null) {
+          subscriber.onNext(currentObject)
+          currentObject = null
+          currentKey = null
+        }
+      case JsonEventType.START_ARRAY =>
+      case JsonEventType.END_ARRAY =>
       case _ =>
-        // Ignore other event types
-        logger.warn(s"Unhandled JSON event type: ${event.`type`()}")
     }
   }
+
   override def handleError(e: Throwable): Unit = {
     logger.error("Error occurred during JSON streaming", e)
     subscriber.onError(e)
   }
+
   override def end(): Unit = {
     logger.info("JSON stream ended")
     subscriber.onCompleted()
   }
 }
-object HandlerHelper {
-  def arrayJsonEventHandler(parser: JsonParser, handler: ReactStreamJsonHandler, event: JsonEvent): Unit = {
-    event.`type`() match {
-      case JsonEventType.START_ARRAY =>
-        parser.objectValueMode()
-      case JsonEventType.VALUE =>
-        handler.handle(event)
-      case JsonEventType.END_ARRAY =>
-        handler.end()
-      case _ =>
-      // Handle other event types if needed
-    }
-  }
 
+object HandlerHelper {
   def parseJsonStream(subscriber: Subscriber[JsonObject]): JsonParser = {
     val parser = JsonParser.newParser()
+    parser.objectValueMode()
     val handler = new ReactHandler(subscriber)
     streamingParser(handler, parser)
   }
 
   def streamingParser(handler: ReactStreamJsonHandler, parser: JsonParser): JsonParser = {
-    parser.handler(event => arrayJsonEventHandler(parser, handler, event))
+    parser.handler(event => handler.handle(event))
     parser.exceptionHandler(e => handler.handleError(e))
     parser.endHandler(_ => handler.end())
-    parser
   }
 }
